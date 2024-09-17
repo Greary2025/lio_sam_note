fd2789f3323b04e913cb86fd8adb138c59c5cd29
grearygreen <2502785143@qq.com>
2024-09-17 14:22:45 +0800

    update README

---
+++ build/CMakeFiles/lio_sam_imuPreintegration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lio_sam_imuPreintegration.dir/src/imuPreintegration.cpp.o"
+  "devel/lib/lio_sam/lio_sam_imuPreintegration"
+  "devel/lib/lio_sam/lio_sam_imuPreintegration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lio_sam_imuPreintegration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/catkin_generated/lio_sam-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(lio_sam_MESSAGE_FILES "/home/john/liosam_ws/src/LIO-SAM/msg/cloud_info.msg")
+set(lio_sam_SERVICE_FILES "/home/john/liosam_ws/src/LIO-SAM/srv/save_map.srv")
